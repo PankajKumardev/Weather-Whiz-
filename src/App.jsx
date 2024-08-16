9d0f44ea64ae33353e62d62907b72ca8e1bbe307
Pankaj Kumar <104948046+PankajKumardev@users.noreply.github.com>
2024-08-16 22:12:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import './App.css'
-import { WeatherBox } from './components/WeatherBox'
+import './App.css';
+import { WeatherBox } from './components/WeatherBox';
 
 function App() {
-
-
   return (
     <div className='flex flex-col justify-center items-center min-h-screen p-4 bg-[#08090A]'>
-    <h1 className='text-4xl font-bol mb-24 text-center'>
-    <span className='text-blue-400'>Weather Whiz</span>
-    <span className='text-slate-300'> : Your Climate Companion</span>
-    </h1>
-    <div className='w-full ml-52'>
+      <h1 className='text-4xl font-bold mb-24 text-center'>
+        <span className='text-blue-400'>Weather Whiz</span>
+        <span className='text-slate-300'> : Your Climate Companion</span>
+      </h1>
+      <div className='w-full max-w-lg px-4'>
         <WeatherBox />
+      </div>
     </div>
-</div>
-
-  )
+  );
 }
 
-export default App
+export default App;
