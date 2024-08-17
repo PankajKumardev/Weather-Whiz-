fc9cb1f54f4feef663569a97fc99142f69f2ab67
Pankaj Kumar <104948046+PankajKumardev@users.noreply.github.com>
2024-08-17 14:57:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import { WeatherBox } from './components/WeatherBox';
 function App() {
   return (
     <div className='flex flex-col justify-center items-center min-h-screen p-4 bg-[#08090A]'>
-      <h1 className='text-4xl font-bold mb-24 text-center'>
-        <span className='text-blue-400'>Weather Whiz</span>
-        <span className='text-slate-300'> : Your Climate Companion</span>
-      </h1>
+      <h2 className="my-10 sm:my-16 text-2xl sm:text-3xl lg:text-5xl text-center font-bold">
+  <span className='text-[#00aaff]'>Weather Whiz</span><br />
+  <span className='text-[#ffbb00]'>Your Climate Companion</span> 🌤️
+</h2>
       <div className='w-full max-w-lg px-4'>
         <WeatherBox />
       </div>
