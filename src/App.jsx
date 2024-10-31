855fd4392029f5dedcf56ad06cf0cb6307961529
pankaj kumar <Pankajams1234@gmail.com>
2024-10-31 16:37:23 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
 import { WeatherBox } from './components/WeatherBox';
-
+import {analytics} from "@vercel/analytics";
 function App() {
   return (
     <div className='flex flex-col justify-center items-center min-h-screen p-4 bg-[#08090A]'>
+       <analytics/>
       <h2 className="my-10 sm:my-16 text-2xl sm:text-3xl lg:text-5xl text-center font-bold">
   <span className='text-[#00aaff]'>Weather Whiz</span><br />
   <span className='text-[#ffbb00]'>Your Climate Companion</span> 🌤️
